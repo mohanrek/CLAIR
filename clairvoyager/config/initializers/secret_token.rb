ac2e9b14abe1f3e27ed3d7e8dd4992154748c051
Mohan Raj EK <mohanraj.ek@gmail.com>
2011-07-03 23:46:51 +0800

    new rails project

---
+++ clairvoyager/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Clairvoyager::Application.config.secret_token = '9432e927e6a83c13c30a039f285a91d073996652a5c539e546c63fd82b9855800e1ee63667c355ffe2414983196edcfa6a1b2c7d05658d81eeb1b026719056d7'
